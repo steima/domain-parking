d4b4933833e4be42d12e6de0d0ec51adc8e82a1c
Matthias Steinbauer <matthias@steinbauer.org>
2017-04-17 19:15:20 +0200

    Removes header from twitter widget

--- index.php
+++ index.php
@@ -77,6 +77,7 @@ $server = $_SERVER ['HTTP_HOST'];
 			</div>
 			<div class="col-md-6">
 				<a class="twitter-timeline" href="https://twitter.com/hashtag/wahl2017"
+					chrome="noheader"
 					data-widget-id="853863854748684288">#wahl2017 Tweets</a>
 				<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 			</div>
