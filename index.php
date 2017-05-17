f2fa2bb5a0f7f80a4bc64c19b010c64416ffbd93
Matthias Steinbauer <matthias@steinbauer.org>
2017-05-17 08:59:48 +0200

    Added more news sources

--- index.php
+++ index.php
@@ -155,6 +155,39 @@
 		<div class="container">
 			<h1><?=$config['heading']?></h1>
 			<p><?=$config['teaser']?></p>
+<!-- Display the countdown timer in an element -->
+<p id="countdown"></p>
+
+<script>
+// Set the date we're counting down to
+var countDownDate = new Date("Oct 15, 2017 08:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get todays date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now an the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
+</script>			
 			<p>
 				<a id="cta" class="btn btn-primary btn-lg" href="<?=$config['mailtocontent']?>" role="button"><span class="<?=$config['teaserctaclass']?>" style="padding-right:10px;"></span><?=$config['teasercta']?></a>
 			</p>
